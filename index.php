3b031c1a15b0d886434708237af9798162d96836
TechHog <77560013+TechHog8984@users.noreply.github.com>
2022-09-07 08:26:25 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
     <title>Hellooo</title>
   </head>
   <body>
-    <p>
-      Hellooo
-    </p>
+    <form>
+    Student Name:
+    <input type="text" name="Name" size="20">
+    <br> <br>
+    Course:
+    <input type="text" name="Course" size="15">
+    </form>
   </body>
 </html>
